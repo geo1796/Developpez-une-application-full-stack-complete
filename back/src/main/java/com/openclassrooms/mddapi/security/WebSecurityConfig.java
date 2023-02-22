ee828ea8fba4e36be0585a7ec32c68e9965036b0
Geoffrey Ouvrard <geoffrey.1796@gmail.com>
2023-02-22 21:12:36 +0100

    added JwtFilter in SpringBoot app

--- back/src/main/java/com/openclassrooms/mddapi/security/WebSecurityConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/security/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -69,7 +70,7 @@ public class WebSecurityConfig {
                 )
                 .httpBasic(withDefaults());
 
-        //http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
